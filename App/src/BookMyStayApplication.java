4a18dfd151cf9fdf7fd722ddfa10828e7b8cdc44
Litudalei <gitiranjan815@gmail.com>
2026-06-18 10:20:55 +0530

    uc1-welcome-message

---
+++ App/src/BookMyStayApplication.java
@@ -0,0 +1,6 @@
+public class BookMyStayApplication {
+    public static void main(String[] args) {
+        System.out.println("welcome to hotel booking management system");
+        System.out.println("System initialized successfully");
+    }
+}
