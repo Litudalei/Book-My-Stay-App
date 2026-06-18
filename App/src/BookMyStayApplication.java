30ffaf627b2c06385be57f0b420b14be7292e8c2
Litudalei <gitiranjan815@gmail.com>
2026-06-18 12:48:46 +0530

    uc4-BookingRequest

--- App/src/BookMyStayApplication.java
+++ App/src/BookMyStayApplication.java
@@ -1,29 +1,21 @@
 public class BookMyStayApplication {
     public static void main(String[] args) {
-        System.out.println("welcome to hotel booking management system");
-        System.out.println("System initialized successfully");
-        RoomInventory inventory = new RoomInventory();
+        System.out.println("==================================");
+        System.out.println(" HOTEL BOOKING MANAGEMENT SYSTEM");
+        System.out.println("           VERSION 4.0");
+        System.out.println("==================================");
 
-        System.out.println("\nInitial Inventory:");
-        inventory.displayInventory();
+        RoomInventory inventory =
+                new RoomInventory();
 
-        System.out.println("\nAvailability Checks:");
-        System.out.println("Single Room : "
-                + inventory.getAvailability("Single Room"));
+        RoomSearchService searchService =
+                new RoomSearchService(inventory);
 
-        System.out.println("Double Room : "
-                + inventory.getAvailability("Double Room"));
+        searchService.displayAvailableRooms();
 
-        System.out.println("Suite Room : "
-                + inventory.getAvailability("Suite Room"));
+        searchService.searchRoom("Single Room");
 
-        System.out.println("\nUpdating Inventory...");
-
-        inventory.updateAvailability("Single Room", 8);
-        inventory.updateAvailability("Double Room", 4);
-
-        System.out.println("\nUpdated Inventory:");
-        inventory.displayInventory();
+        searchService.searchRoom("Suite Room");
 
         System.out.println("\nApplication Terminated.");
 
