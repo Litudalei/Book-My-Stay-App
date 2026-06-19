730ad841ea86a48b0ced84b2d35bf4fcc5e65aea
Litudalei <gitiranjan815@gmail.com>
2026-06-19 10:49:36 +0530

    uc8-Booking-History-Report

--- App/src/DoubleRoom.java
+++ App/src/DoubleRoom.java
@@ -1,14 +1,23 @@
-class DoubleRoom extends Room {
+
+public class DoubleRoom extends Room {
 
     public DoubleRoom() {
-        super("Double Room", 2, 250.0, 3500.0);
+
+        super(
+                "D101",
+                "Double Room",
+                2,
+                250.0,
+                3500.0
+        );
     }
 
     @Override
     public void displayRoomDetails() {
-        System.out.println("Room Type      : " + getRoomType());
-        System.out.println("Beds           : " + getNumberOfBeds());
-        System.out.println("Room Size      : " + getRoomSize() + " sq.ft");
-        System.out.println("Price Per Night: ₹" + getPricePerNight());
+
+        System.out.println("Room Type : " + getRoomType());
+        System.out.println("Beds : " + getNumberOfBeds());
+        System.out.println("Room Size : " + getRoomSize());
+        System.out.println("Price : ₹" + getPricePerNight());
     }
 }
